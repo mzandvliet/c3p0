87b38a09b2eb81b382de8d0aad0d752fd5deef9f
Martijn Zandvliet <zandvliet.martijn@gmail.com>
2013-11-14 23:32:00 +0100

    - Updated MacdSource todo notes

--- src/main/java/c3po/MacdSource.java
+++ src/main/java/c3po/MacdSource.java
@@ -20,7 +20,19 @@ public class MacdSource implements ICompositeSignal {
 	public MacdSource(ISignal ticker, int fast, int slow, int signal) {
 		this.signals = new SurrogateSignal[numSignals];
 		
-		// Todo: create transformation buffers for each stage
+		/* Todo: 
+		 * 
+		 * - create internal signal tree (mov. avg. etc.), hook them up together
+		 * 
+		 * From the javascript original:
+		 * 
+		 * var avgA = filterExpMovingAverage_(ticker, fast);
+  		 * var avgB = filterExpMovingAverage_(ticker, slow);
+  		 * var macd = combine_(avgA, avgB, function(a, b) { return a - b; });
+  		 * var macdAvg = filterExpMovingAverage_(macd, signal);
+  		 * var diff = combine_(macd, macdAvg, function(a, b) { return a - b; });
+		 */
+		
 		this.signals[0] = new SurrogateSignal(this);
 		this.signals[1] = new SurrogateSignal(this);
 		this.signals[2] = new SurrogateSignal(this);
@@ -46,7 +58,12 @@ public class MacdSource implements ICompositeSignal {
 	}
 	
 	private void calculate() {
-		
+		/*
+		 * Todo:
+		 *  
+		 * - Draw new Sample through internal signal tree
+		 * - Send Sample to output signals
+		 */
 	}
 	
 	public enum SignalNames {
