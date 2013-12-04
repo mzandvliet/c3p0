fb39f9d6dc9036fa2325b37a104dec03177cf09f
Martijn Zandvliet <zandvliet.martijn@gmail.com>
2013-12-04 16:13:05 +0100

    - New version for Tatooine - Removed graphing node and some fluff from RealtimeRunner

--- src/main/java/c3po/production/RealtimeBotRunner.java
+++ src/main/java/c3po/production/RealtimeBotRunner.java
@@ -4,7 +4,6 @@ import c3po.*;
 
 import java.net.InetSocketAddress;
 import java.sql.SQLException;
-import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +12,6 @@ import c3po.bitstamp.BitstampTickerSource;
 import c3po.bitstamp.BitstampTickerDbSource;
 import c3po.bitstamp.BitstampTradeFloor;
 import c3po.EmailTradeLogger;
-import c3po.GraphingNode;
 import c3po.IClock;
 import c3po.ITradeFloor;
 import c3po.Time;
@@ -26,11 +24,6 @@ public class RealtimeBotRunner {
 
 	private final static long interpolationTime = 2 * Time.MINUTES;
 	private final static long timestep = 1 * Time.MINUTES;
-	
-	private static final double walletDollarStart = 0.0;
-	private static final double walletBtcStart = 0.0;
-	
-	private final static long graphInterval = 1 * Time.MINUTES;
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 			
@@ -54,7 +47,7 @@ public class RealtimeBotRunner {
 		/**
 		 * Tradefloor + Wallet Initialization
 		 */
-		final IWallet wallet = new Wallet(walletDollarStart, walletBtcStart);
+		final IWallet wallet = new Wallet(0d, 0d);
 		
 		final ITradeFloor tradeFloor =  new BitstampTradeFloor(
 				tickerNode.getOutputLast(),
@@ -77,36 +70,13 @@ public class RealtimeBotRunner {
 		EmailTradeLogger mailLogger = new EmailTradeLogger("martijn@ramjetanvil.com", "jopast@gmail.com");
 		bot.addTradeListener(mailLogger);
 		
-		// Graph performance in realtime
-		
-		GraphingNode grapher = new GraphingNode(graphInterval, "Ticker", 
-				tickerNode.getOutputLast()
-				);
-		bot.addTradeListener(grapher);
-		
-		GraphingNode diffGrapher = new GraphingNode(graphInterval, "Macd", 
-				bot.getAnalysisNode().getOutputDifference()
-				);
-		bot.addTradeListener(diffGrapher);
-		
-		grapher.pack();
-		grapher.setVisible(true); // Show graph *after* simulation because otherwise annotation adding causes exceptions
-		
-		diffGrapher.pack();
-		diffGrapher.setVisible(true); // Show graph *after* simulation because otherwise annotation adding causes exceptions
-		
 		// Create a clock
 		IClock botClock = new RealtimeClock(timestep, Math.max(analysisConfig.slowPeriod, analysisConfig.signalPeriod), interpolationTime);
 		botClock.addListener(bot);
-		botClock.addListener(grapher);
-		botClock.addListener(diffGrapher);
 		
 		// Run the program
 		tickerNode.open();
 		botClock.run();
 		tickerNode.close();
-		
-		// Log results
-		LOGGER.debug("Num trades: " + tradeLogger.getActions().size() + ", Profit: " + (tradeFloor.getWalletValueInUsd(wallet) - walletDollarStart));
 	}
 }
