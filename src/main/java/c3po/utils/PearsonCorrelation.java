b4eb50f5bb008ba38680b6a8a0bd7d7c77731022
Joost Pastoor <jopast@gmail.com>
2013-12-15 16:37:18 +0100

    Worked on correlation the order book aggregates with the ticker.

--- src/main/java/c3po/utils/PearsonCorrelation.java
+++ src/main/java/c3po/utils/PearsonCorrelation.java
@@ -1,7 +1,58 @@
 package c3po.utils;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+
+import au.com.bytecode.opencsv.CSVReader;
+
 public class PearsonCorrelation {
-	public static double getPearsonCorrelation(double[] scores1, double[] scores2) {
+	
+	public static void main(String[] args) throws IOException {
+		
+		CSVReader reader = new CSVReader(new FileReader("resources/correlate_me.csv"));
+		
+		List<String[]> readAll = reader.readAll();
+
+		// Initialize arrays
+		int signalCount = SignalName.values().length;
+		Double[][] data = new Double[signalCount][readAll.size()];
+		
+		// Put the CSV data in the arrays
+		for(int line = 0; line < readAll.size(); line++) {
+			for(int i = 0; i < signalCount; i++)
+			data[i][line] = Double.valueOf(readAll.get(line)[i]); 
+		}
+		
+		// Combinations that we wish to try		
+		List<SignalName> correlationProperties = Arrays.asList(new SignalName[] {SignalName.volume_bid, SignalName.volume_ask, SignalName.p99_bid});
+		
+		for(SignalName signal1 : correlationProperties) {
+			for(SignalName signal2 : correlationProperties) {
+				// Dont correlate with self
+				if(signal1 == signal2) continue;
+				
+				double pearsonCorrelation = getPearsonCorrelation(data[signal1.ordinal()], data[signal2.ordinal()]);
+				System.out.println("Correlation between " + signal1 + " and " + signal2 + " = " + pearsonCorrelation);
+			}
+		}
+
+		
+		double pearsonCorrelation = getPearsonCorrelation(data[SignalName.last.ordinal()], data[SignalName.bid.ordinal()]);
+		System.out.println(pearsonCorrelation);
+	}
+	
+	public static double getPearsonCorrelation(Double[] scores1, Double[] scores2) {
 		double result = 0;
 		double sum_sq_x = 0;
 		double sum_sq_y = 0;
@@ -23,5 +74,9 @@ public class PearsonCorrelation {
 		double cov_x_y = sum_coproduct / scores1.length;
 		result = cov_x_y / (pop_sd_x * pop_sd_y);
 		return result;
+	}
+	
+	public enum SignalName {
+		timestamp_ob, volume_bid, volume_ask, p99_bid, p99_ask, p98_bid, p98_ask, p97_bid, p97_ask, p96_bid, p96_ask, p95_bid, p95_ask, p90_bid, p90_ask, p85_bid, p85_ask, p80_bid, p80_ask, p75_bid, p75_ask, timestamp_ticker, high, last, bid, volume, low, ask;
 	}
 }
