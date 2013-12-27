ed5e6366925ad3a1bc0dafd50d1ad02265e860e6
Joost Pastoor <jopast@gmail.com>
2013-12-27 14:55:11 +0100

    Removed unnessacary stuff

--- src/main/java/c3po/bitstamp/BitstampSimulationTickerDbSource.java
+++ src/main/java/c3po/bitstamp/BitstampSimulationTickerDbSource.java
@@ -1,7 +1,5 @@
 package c3po.bitstamp;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,26 +11,18 @@ import c3po.db.DbTimeseriesSource;
 
 public class BitstampSimulationTickerDbSource extends BitstampTickerSource implements INonRealtimeSource {
 	private static final Logger LOGGER = LoggerFactory.getLogger(BitstampSimulationTickerDbSource.class);
-	private static final int MAXRETRIES = 3;
 
-	private DbConnection connection;
-		
 	private long simulationStartTime;
 	private long simulationEndTime;
-	
-	private int lastDataIndex;
 	
 	private DbTimeseriesSource source;
 	
 	public BitstampSimulationTickerDbSource(long timestep, long interpolationTime, DbConnection connection, long dataStartTime, long dataEndTime) {
 		  super(timestep, interpolationTime);
 		  
-		  this.connection = connection;
-		  
 		  this.source = new DbTimeseriesSource(timestep, connection, "bitstamp_ticker", Arrays.asList("last", "high", "low", "volume", "bid", "ask"));
 		  this.source.fetchDataFromDatabase(dataStartTime, dataEndTime);
 
-		  
 		  setSimulationRange(dataStartTime, dataEndTime); // Default the simulation to the full range of data
 	}
 	
